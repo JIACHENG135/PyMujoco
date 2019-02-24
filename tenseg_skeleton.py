2cde80ce9cdb59f1b599f40db0a7d6157bb5ac30
JIACHENG135 <liulaoye135@tamu.edu>
2019-02-24 11:25:43 -0600

    new skeleton

--- tenseg_skeleton.py
+++ tenseg_skeleton.py
@@ -9,38 +9,38 @@ class tenseg_skeleton(object):
     def __init__(self,data_name):
         self.mem = {}
         self.bar_mem = collections.defaultdict(set)
+        # self.bar_mem["0 0 0"]
         self.p2end = {}
         self.data = scio.loadmat(data_name)
         self.Cb = self.data['C_b']
         self.Cs = self.data['C_s']
         self.N = self.data['N']
+        self.color = ["1 0 0 .8","1 0 0 .8","1 0 0 .8","1 0 0 .8","1 0 0 .8","1 0 0 .8"];
         self.worldbody = worldbody()
         Geom = geom(name="floor",pos="0 0 -10",size="0 0 1",type="plane",material="matplane")
         Light = light(directional="true",diffuse=".8 .8 .8",specular=".2 .2 .2",pos="0 0 5",dir="0 0 -1")
         Site = site(name="s0",pos="0 0 0",size="0.1")
         Body = body(name = 'floor',pos = '0 0 0')
+        # ball = body(name = "ball",pos="0 0 0")
+        bgeom = geom(name = "ball1",pos="0 0 0",size = "0.11",type="sphere",material="matplane")
+        # ball<<bgeom
+# <!--     <body name = 'ball' pos = '4. 0. 0.'>
+#         <geom name="ball1" pos="0 0 0" size="0.11" type="sphere" material="matplane"/>
+#     </body> -->
         Body<<Geom
-        Body<<Light
+        self.worldbody<<Light
         Body<<Site
         self.worldbody<<Body
+        # self.worldbody<<ball
         self.strcounter = 1
         self.front = '''<mujoco model="dbar">
-
-    <option timestep="0.01" iterations="50" solver="Newton" tolerance="1e-10" gravity = "0 0 0" collision="predefined" viscosity=".5"/>
-
-    <size njmax="5000" nconmax="1000" nstack="20000"/>
-
-    <visual>
-        <rgba haze=".3 .3 .3 1"/>
-    </visual>
+    <option timestep="0.01" integrator="RK4" gravity = "0 0 0" collision="predefined" density="1000"/>
 
     <default>
-        <position ctrllimited="false" ctrlrange="-1 1" gear="1.0"/>
-        <tendon damping="2" rgba = ".8 .8 .8 .5"/>
-        <geom size="0.01"/>
-        <site size="0.01"/>
+        <geom size="0.03"/>
+        <site size="0.05"/>
     </default>
-
+    
     <asset>
         <texture type="skybox" builtin="gradient" rgb1="0.6 0.6 0.6" rgb2="0 0 0" width="512" height="512"/> 
 
@@ -153,15 +153,15 @@ class tenseg_skeleton(object):
 
             # to build body and it's child tag, joint geom and site
             bodyname = 'body' + str(i)
-            self.bar_mem[str(start)[1:-1]].add(bodyname)
-            self.bar_mem[globalend].add(bodyname)
+            self.bar_mem[str(start)[1:-1]].add((bodyname,0))
+            self.bar_mem[globalend].add((bodyname,1))
             self.p2end[bodyname] = end
-
+            # self.p2start[bodyname] = pos
             locals()["Body"+str(i)] = body(name = "body"+str(i),pos=pos)
             tmpj = joint(type="free",pos="0 0 0")
             command = "Body"+str(i) + "<<tmpj"
             eval(command)
-            tmpg = geom(name ="body"+str(i),type = "capsule",fromto="0 0 0 "+ end ,rgba=".5 .1 .1 1")
+            tmpg = geom(name ="body"+str(i),type = "capsule",fromto="0 0 0 "+ end ,rgba=self.color[i%5])
             command = "Body"+str(i) + "<<tmpg"
             eval(command)
 
@@ -216,13 +216,25 @@ class tenseg_skeleton(object):
         equ = equality()
         ct = 1
         for i in self.bar_mem:
+            # print(i,self.bar_mem[i])
             if len(list(self.bar_mem[i]))>=2:
-                s = list(self.bar_mem[i])[0]
+                s = list(self.bar_mem[i])[0][0]
+                # print(s)
+                flag = list(self.bar_mem[i])[0][1]
+                if flag:
+                    anc = self.p2end[s]
+                else:
+                    anc = '0 0 0'
+                # print(anc)
                 for j in list(self.bar_mem[i])[1:]:
-                    tmpc = connect(active = 'true',name = s + '2' + j,body1 = s,body2 = j,anchor=self.p2end[s])
+                    print(s,j[0],i,self.p2end[s],self.p2end[j[0]])
+                    tmpc = connect(active = 'true',name = s + '2' + j[0],body1 = s,body2 = j[0],anchor=anc)
                     command = 'equ<<tmpc'
                     eval(command)
 
+        # tmpc = connect(active = 'true',name = "floor" + '2' + "body0",body1 = "floor",body2 = "body0",anchor="0 0 0")
+        # command = "equ<<tmpc"
+        # eval(command)
 
         wait = str(equ.render())
         n = len(wait)
@@ -242,7 +254,7 @@ class tenseg_skeleton(object):
         act = actuator()
         for i in actuatorlist:
             print(i)
-            tmpp = position(tendon = 'S' + str(i),kp = '1')
+            tmpp = position(tendon = 'S' + str(i),kp = '0')
             command = 'act<<tmpp'
             eval(command)
         wait = str(act.render())
